3a51be3bf5c5fa1e59885466ec46a9aa3fcc2633
Khushal Mali <khushalmali8448@gmail.com>
2024-07-06 15:40:34 +0530

    reusable form

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -12,8 +14,28 @@ export default function Home() {
             alt="patient"
             className="h-12 mb-12 w-fit"
           />
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              &copy; 2024 CarePulse
+            </p>
+
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
